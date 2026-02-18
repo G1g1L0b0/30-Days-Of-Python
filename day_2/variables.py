29dcd301258ea0672073407dece3edd0126eec32
G1g1L0b0 <liviobixheku1@gmail.com>
2026-02-18 22:55:27 +0200

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,21 @@
 # Day 2: 30 Days of python programming
+first_name=input("What's ypur name: ")
+last_name=input("What's your name: ")
+full_name=f"{first_name} {last_}"
+country=input("Where are you from: ")
+age=input("How old are you: ")
+is_married=input("Are you married? Yes or No: ")
+print(len(first_name))
+num_one=5
+num_two=4
+total=num_one + num_two
+diff=num_two - num_one
+product=num_one*num_two
+division=num_one/num_two
+remainer=num_one%num_two
+exp=num_one^num_two
+floor_division=num_one//num_two
+r=input("Give a radius for a circle") #iii
+area_of_circle=2*pi*r^2 #i
+circum_of_circle=2*pi*r #ii
+help('keywords')
